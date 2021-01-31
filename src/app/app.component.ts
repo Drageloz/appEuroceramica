8eac220ad155e4f506c908bed650a9ecb07ba82c
drageloz <drageloz@gmail.com>
2021-01-30 23:00:15 -0500

    actualización a estado inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Routes } from '@angular/router';
 
 @Component({
@@ -8,4 +8,17 @@ import { Routes } from '@angular/router';
 })
 export class AppComponent {
   title = 'views';
+
+  //Se agrega el hostListener para que submenu se desactive cuando se haga click
+  @HostListener('click', ['$event'])
+  unselect(event: any){      
+      const submenusActives = document.getElementsByClassName("container_slidebar-submenu") as HTMLCollectionOf<HTMLElement>;
+      for(let i = 0; i< submenusActives.length; i++){
+        if(submenusActives[i].style.display == "block"){
+          console.log("entra");
+          submenusActives[i].style.display = "none";
+        }
+      };
+  } 
+
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,20 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.sass']
+  styleUrls: ['./menu.component.sass'],
+  
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements AfterViewInit {
 
-  constructor(public router: Router) { }
+  
 
-  ngOnInit(): void {
+  constructor(public router: Router, private elementRef:ElementRef) {
   }
 
+  ngAfterViewInit(): void {
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  //activa el color del menu y realiza el llamado de la función para activar el submenú
+  async active(menu: HTMLElement, submenu: HTMLElement){
+    if(submenu.style.display == "block"){
+      submenu.style.display = "none";
+      return;
+    }
+    //se agrea un delay debido a que en app component el @hostlistener actua muy rápido y bloquea la muestra del submenu
+    await this.delay(10);
+    const buttonsActives = document.getElementsByClassName("container_slidebar--active");
+    for(let i = 0; i< buttonsActives.length; i++){
+      buttonsActives[i].className = "container_slidebar_menu container_slidebar" + "--" + buttonsActives[i].id;
+    }
+    menu.className = "container_slidebar_menu container_slidebar" + "--" + menu.id + " container_slidebar--active";
+    this.activeSubmenu(menu, submenu);
+
+    
+  }
+
+  //activa el submenu luego de dar click sobre el menú
+  activeSubmenu(menu: HTMLElement, submenu: HTMLElement){
+      
+      const submenusActives = document.getElementsByClassName("container_slidebar-submenu") as HTMLCollectionOf<HTMLElement>;
+      for(let i = 0; i< submenusActives.length; i++){
+        submenusActives[i].style.display = "none";
+      }
+      //utiliza las propiedades del top y right del menú para definir la posición del submenú enfrente del menu
+      submenu.style.top = menu.getBoundingClientRect().top + "px";
+      submenu.style.left = (menu.getBoundingClientRect().right + 5) + "px";
+      console.log(submenu.style.display);
+      submenu.style.display = "block";
+  }
+
+
+  //deslogea al usuario
   async logout(){
     localStorage.setItem("sesion", "inactiva");
     this.router.navigate(['/login']);
   }
+
+
+  
+
 }
